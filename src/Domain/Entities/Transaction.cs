ea3edaed8b3d9e314012628fc4a39e66921cf400
aNNiPAk <annipak@yandex.ru>
2023-08-07 23:47:08 +0300

    feat: added class transaction

---
+++ src/Domain/Entities/Transaction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace Domain.Entities;
+
+public class Transaction : IEnumerable<Item>
+{
+    public IList<Item> Items { get; }
+    public int Id { get; }
+    public int ClusterId { get; set; } = -1;
+    public int ItemCount => Items.Count;
+
+    private static int _transactionId;
+
+    public Transaction(IList<Item> items)
+    {
+        Items = items;
+        Id = _transactionId++;
+    }
+
+    public Transaction(IEnumerable<string> items) : this(items.Select(item => new Item(item)).ToList())
+    {
+    }
+
+    public Transaction(params string[] items) : this(items.Select(item => new Item(item)).ToList())
+    {
+    }
+
+    public IEnumerator<Item> GetEnumerator()
+    {
+        return Items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public override string ToString()
+    {
+        return "{" + string.Join(", ", Items) + "}";
+    }
+}
---
+++ tests/Domain.Tests/Entities/TransactionTests.cs
@@ -0,0 +1,76 @@
+﻿using Domain.Entities;
+
+namespace Domain.Tests.Entities;
+
+public class TransactionTests
+{
+    [Fact]
+    public void TestTransactionConstructor()
+    {
+        // Arrange
+        var items = new List<Item> { new("item1"), new("item2") };
+        
+        // Act
+        var transaction = new Transaction(items);
+        
+        // Assert
+        Assert.Equal(items, transaction.Items);
+        Assert.Equal(-1, transaction.ClusterId);
+    }
+
+    [Fact]
+    public void TestTransactionConstructorWithStrings()
+    {
+        // Arrange
+        var items = new List<string> { "item1", "item2" };
+        
+        // Act
+        var transaction = new Transaction(items);
+        
+        // Assert
+        Assert.Equal(items.Count, transaction.Items.Count);
+        Assert.Equal(-1, transaction.ClusterId);
+    }
+
+    [Fact]
+    public void TestTransactionConstructorWithParams()
+    {
+        // Act
+        var transaction = new Transaction("item1", "item2");
+        
+        // Assert
+        Assert.Equal(2, transaction.Items.Count);
+        Assert.Equal(-1, transaction.ClusterId);
+    }
+
+    [Fact]
+    public void GetEnumeratorTest()
+    {
+        // Arrange
+        var items = new List<Item> { new("item1"), new("item2") };
+        var transaction = new Transaction(items);
+        
+        // Act
+        using var enumerator = transaction.GetEnumerator();
+        
+        // Assert
+        Assert.True(enumerator.MoveNext());
+        Assert.Equal(items[0], enumerator.Current);
+        Assert.True(enumerator.MoveNext());
+        Assert.Equal(items[1], enumerator.Current);
+        Assert.False(enumerator.MoveNext());
+    }
+
+    [Fact]
+    public void ToStringTest()
+    {
+        // Arrange
+        var items = new List<Item> { new("item1"), new("item2") };
+        
+        // Act
+        var transaction = new Transaction(items);
+        
+        // Assert
+        Assert.Equal("{item1, item2}", transaction.ToString());
+    }
+}
