aa278bee5b82cd53938136ccff9bd544ac3cdbc1
aNNiPAk <annipak@yandex.ru>
2023-08-07 23:46:33 +0300

    feat: added item class for transactions

---
+++ src/Domain/Entities/Item.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Entities;
+
+public class Item
+{
+    public string Value { get; }
+    public int Hash { get; }
+
+    public Item(string value)
+    {
+        Value = value;
+        Hash = value.GetHashCode();
+    }
+
+    public override int GetHashCode()
+    {
+        return Hash;
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        return obj is Item item && Value == item.Value;
+    }
+
+    public override string ToString()
+    {
+        return Value;
+    }
+}
---
+++ tests/Domain.Tests/Entities/ItemTests.cs
@@ -0,0 +1,81 @@
+﻿using Domain.Entities;
+
+namespace Domain.Tests.Entities;
+
+public class ItemTests
+{
+    [Fact]
+    public void Constructor_SetsValueAndHash()
+    {
+        // Arrange
+        var value = "test";
+        var expectedHash = value.GetHashCode();
+
+        // Act
+        var item = new Item(value);
+
+        // Assert
+        Assert.Equal(value, item.Value);
+        Assert.Equal(expectedHash, item.Hash);
+    }
+
+    [Fact]
+    public void GetHashCode_ReturnsHash()
+    {
+        // Arrange
+        var value = "test";
+        var expectedHash = value.GetHashCode();
+        var item = new Item(value);
+
+        // Act
+        var hash = item.GetHashCode();
+
+        // Assert
+        Assert.Equal(expectedHash, hash);
+    }
+
+    [Fact]
+    public void Equals_ReturnsTrueForEqualItems()
+    {
+        // Arrange
+        var value = "test";
+        var item1 = new Item(value);
+        var item2 = new Item(value);
+
+        // Act
+        var result = item1.Equals(item2);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Equals_ReturnsFalseForDifferentItems()
+    {
+        // Arrange
+        var value1 = "test1";
+        var value2 = "test2";
+        var item1 = new Item(value1);
+        var item2 = new Item(value2);
+
+        // Act
+        var result = item1.Equals(item2);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ToString_ReturnsValue()
+    {
+        // Arrange
+        var value = "test";
+        var item = new Item(value);
+
+        // Act
+        var result = item.ToString();
+
+        // Assert
+        Assert.Equal(value, result);
+    }
+}
